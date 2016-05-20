cc12e84b794e3549822a8f199c5428534bb8a11e
lianchenfeng <1289161992@qq.com>
2016-05-20 11:49:25 +0800

    完成了基本配置

---
+++ src/main/java/com/it/pojo/User.java
@@ -0,0 +1,84 @@
+package com.it.pojo;
+
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+
+    public static final String USER_STATE_OK = "正常";
+    public static final String USER_STATE_DISABLE = "禁用";
+    public static final String SESSION_KEY = "curr_user";
+
+    private Integer id;
+    private String username;
+    private String password;
+    private String name;
+    private String tel;
+    private String state;
+    private String createtime;
+    private Integer roleid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(String createtime) {
+        this.createtime = createtime;
+    }
+
+    public Integer getRoleid() {
+        return roleid;
+    }
+
+    public void setRoleid(Integer roleid) {
+        this.roleid = roleid;
+    }
+}
---
+++ src/main/java/com/it/service/UserService.java
@@ -0,0 +1,7 @@
+package com.it.service;
+
+/**
+ * Created by hp on 2016/5/20.
+ */
+public class UserService {
+}
